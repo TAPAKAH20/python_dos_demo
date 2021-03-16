d887d0d9ffa55a4d3712f2df7a50cc8277352852
TAPAKAH20 <tapakah20@ya.ru>
2021-03-16 19:00:16 +0300

    return codes fixed

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import subprocess
 
-print('Trigger return code -6 in a subprocess')
+print('Trigger return code other than 1 and 0 in the subprocess')
 
 
-proc = subprocess.Popen(["python3", "subproc.py"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+proc = subprocess.Popen(["python3", "subproc.py"], stdin=subprocess.PIPE)
 res = proc.communicate(input().encode('ASCII'))
 poll = proc.poll()
 
-while poll != -6:
+while poll == 0 or poll == 1:
 	print('Return code was', poll)
-	proc = subprocess.Popen(["python3", "subproc.py"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	proc = subprocess.Popen(["python3", "subproc.py"], stdin=subprocess.PIPE)
 	res = proc.communicate(input().encode('ASCII'))
 	poll = proc.poll()
 
