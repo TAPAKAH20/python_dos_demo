009c207c4ec7465066f4474925c25606e6a04766
TAPAKAH20 <tapakah20@ya.ru>
2021-03-16 18:56:41 +0300

    fixed

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import subprocess
 print('Trigger return code -6 in a subprocess')
 
 
-proc = subprocess.Popen(["python3", "a.py"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+proc = subprocess.Popen(["python3", "subproc.py"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 res = proc.communicate(input().encode('ASCII'))
 poll = proc.poll()
 
 while poll != -6:
 	print('Return code was', poll)
-	proc = subprocess.Popen(["python3", "a.py"], stdin=subprocess.PIPE)
+	proc = subprocess.Popen(["python3", "subproc.py"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 	res = proc.communicate(input().encode('ASCII'))
 	poll = proc.poll()
 
